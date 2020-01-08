a5459cc26f29da362cc75a786e1b3866a2aa8049
Sean Hall <hallofsean@gmail.com>
2020-01-08 15:49:57 +1000

    Updates

---
+++ precache-manifest.875c485ce221fb5f317a0b2d45ea21a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c59c9011f68a2bf8e29999ed6c495b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e8815ece2d68f66ecb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9bc206a4c92299f42db1",
+    "url": "/static/js/2.6bd413a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6bd413a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86e8815ece2d68f66ecb",
+    "url": "/static/js/main.2be36fd6.chunk.js"
+  },
+  {
+    "revision": "aa9cd3caf36c26d4f89c",
+    "url": "/static/js/runtime-main.7a9ef57c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
